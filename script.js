a4d0b8957eb6c366a04d5943ce303ba8b677f427
WagnerCNS <wagcostvez@gmail.com>
2023-02-27 15:20:32 -0300

    Organizando o arquivo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let number1=prompt('Digite o primeiro número: ')
-
 let number2=prompt('Digite o segundo número ')
 
 number1 = Number(number1)
 number2 = Number(number2)
+
 let add = (number1 + number2)
 let sub = (number1 - number2)
 let mult = (number1 * number2)
@@ -24,7 +24,6 @@ if(evenNumber){
     alert('A soma dos dois números é par: '+add)
 }else{
     alert('A soma dos dois números é impar: '+add)
-    
 }
 
 if(equalNumber){
